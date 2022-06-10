eadf1448a9e2b51219368137c176a4313c1fa55e
王伟 <872937094@qq.com>
2022-06-10 19:11:53 +0800

    ✨ feat(container): 增加折叠展开图标及定义点击事件

--- src/components/container/src/index.vue
+++ src/components/container/src/index.vue
@@ -1,27 +1,35 @@
 <script setup lang='ts'>
 import { ref } from 'vue';
-let isCollapse = ref(true)
+let isCollapse = ref<boolean>(false)
+const toggleMenu = () => {
+  isCollapse.value = !isCollapse.value
+}
 </script>
 <template>
   <el-container>
     <el-aside>
       <el-menu :collapse="isCollapse" default-active="2" class="el-menu-vertical-demo">
         <el-menu-item index="1">
-          <el-icon-menu></el-icon-menu>
+          <el-icon-menu class="el-icon-menu"></el-icon-menu>
           <span>导航1</span>
         </el-menu-item>
         <el-menu-item index="2">
-          <el-icon-menu></el-icon-menu>
+          <el-icon-menu class="el-icon-menu"></el-icon-menu>
           <span>导航2</span>
         </el-menu-item>
         <el-menu-item index="3">
-          <el-icon-menu></el-icon-menu>
+          <el-icon-menu class="el-icon-menu"></el-icon-menu>
           <span>导航3</span>
         </el-menu-item>
       </el-menu>
     </el-aside>
     <el-container>
-      <el-header>Header</el-header>
+      <el-header>
+        <span @click="toggleMenu">
+          <el-icon-expand v-if="!isCollapse" style="margin-right:10px"></el-icon-expand>
+          <el-icon-fold v-else></el-icon-fold>
+        </span>
+      </el-header>
       <el-main>
         <router-view></router-view>
       </el-main>
