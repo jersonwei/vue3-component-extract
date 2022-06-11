13276f11979bad93ac3753840a88e2004fef32e5
王伟 <872937094@qq.com>
2022-06-11 17:44:09 +0800

    ✨ feat(chooseArea): 选定区域后向父组件传递数据

--- src/components/chooseArea/src/index.vue
+++ src/components/chooseArea/src/index.vue
@@ -8,6 +8,11 @@ export interface AreaItem {
   code: string
   children?: AreaItem[]
 }
+// 传递给父组件的数据类型
+export interface EmitData {
+  name: string
+  code: string
+}
 // 下拉框省的值
 const province = ref<string>('')
 // 下拉框市的值
@@ -15,9 +20,11 @@ const city = ref<string>('')
 // 下拉框区域的值
 const area = ref<string>('')
 // 拷贝一份原始值
-let areaData = ref<AreaItem[]>(AreaData)
+let allData = ref(AreaData)
 // 对应省份下的城市的数据
 let selectCity = ref<AreaItem[]>([])
+// 给父组件分发时间
+let emits = defineEmits(['change'])
 // computed(() => {
 //   if (!province.value) return []
 //   else {
@@ -25,9 +32,10 @@ let selectCity = ref<AreaItem[]>([])
 //     return cities
 //   }
 // })
+// 监听省份的值
 watch(() => province.value, val => {
   if (val) {
-    let cities = areaData.value.find(val => val.code === province.value)!.children!
+    let cities = allData.value.find(val => val.code === province.value)!.children!
     selectCity.value = cities
   }
   city.value = ''
@@ -42,6 +50,7 @@ let selectArea = ref<AreaItem[]>([])
 //     return areas
 //   }
 // })
+// 监听城市下的值
 watch(() => city.value, val => {
   if (val) {
     let areas = selectCity.value.find(val => val.code === city.value)!.children!
@@ -49,12 +58,34 @@ watch(() => city.value, val => {
   }
   area.value = ''
 })
+// 监听区域的值
+watch(() => area.value, val => {
+  let emitProvinceData: EmitData = {
+    name: province.value && AreaData.find(item => item.code === province.value)!.name,
+    code: province.value
+  }
+  let emitCityData: EmitData = {
+    name: city.value && selectCity.value.find(item => item.code === city.value)!.name,
+    code: city.value
+  }
+  let emitAreaData: EmitData = {
+    name: val && selectArea.value.find(item => item.code === val)!.name,
+    code: val
+  }
+  if (val) {
+    emits('change', {
+      province: emitProvinceData,
+      city: emitCityData,
+      area: emitAreaData
+    })
+  }
+})
 console.log(AreaData)
 </script>
 <template>
   <div>
     <el-select clearable placeholder="请选择省份" v-model="province">
-      <el-option v-for="item in areaData" :key="item.code" :value="item.code" :label="item.name"></el-option>
+      <el-option v-for="item in allData" :key="item.code" :value="item.code" :label="item.name"></el-option>
     </el-select>
     <el-select clearable :disabled="!province" placeholder="请选择城市" v-model="city">
       <el-option v-for="item in selectCity" :key="item.code" :value="item.code" :label="item.name"></el-option>
--- src/views/chooseArea/index.vue
+++ src/views/chooseArea/index.vue
@@ -1,8 +1,11 @@
 <script setup lang='ts'>
 import ChooseArea from "@/components/chooseArea/src/index.vue"
+let changeArea = (val: any) => {
+  console.log(val)
+}
 </script>
 <template>
-  <choose-area></choose-area>
+  <choose-area @change="changeArea"></choose-area>
 </template>
 <style>
 </style>
