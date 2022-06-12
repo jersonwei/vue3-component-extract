4a514741f4bd67c1a474f62faf17a784c5e86735
王伟 <872937094@qq.com>
2022-06-13 01:49:09 +0800

    ✨ feat(chooseCity): 完成字母和城市的基本渲染和样式

--- src/components/chooseCity/src/index.vue
+++ src/components/chooseCity/src/index.vue
@@ -1,5 +1,6 @@
 <script setup lang='ts'>
 import { ref } from 'vue';
+import citys from '../lib/city'
 let result = ref<string>('请选择')
 let visible = ref<boolean>(false)
 let radioVal = ref<string>('按城市')
@@ -34,9 +35,7 @@ const showPoper = () => visible.value = !visible.value
       <div class="result" @click="showPoper">
         <div>{{ result }}</div>
         <div>
-          <transition>
-            <el-icon-arrowdown></el-icon-arrowdown>
-          </transition>
+          <el-icon-arrowdown :class="{ rotate: visible }"></el-icon-arrowdown>
         </div>
       </div>
     </template>
@@ -54,6 +53,26 @@ const showPoper = () => visible.value = !visible.value
           </el-select>
         </el-col>
       </el-row>
+      <div class="city">
+        <!-- <div v-for="(value, key) in citys.cities"></div> -->
+        <div class="city-item" v-for="(item, index) in Object.keys(citys.cities)" :key="index">{{ item }}</div>
+      </div>
+      <el-scrollbar max-height="300px">
+        <template v-for="(value, key) in citys.cities" ::key="key">
+          <el-row style="marginBottom:10px">
+            <el-col :span='2'>
+              {{ key }}
+            </el-col>
+            <el-col :span="22" class="city-name">
+              <div class="city-name-item" v-for="item in value" :key="item.id">
+                <div>
+                  {{ item.name }}
+                </div>
+              </div>
+            </el-col>
+          </el-row>
+        </template>
+      </el-scrollbar>
     </div>
   </el-popover>
 </template>
@@ -66,12 +85,47 @@ const showPoper = () => visible.value = !visible.value
   cursor: pointer;
 
   svg {
+    position: relative;
     width: 1em;
     height: 1em;
+    top: 2px;
+    margin-left: 4px;
+    transition: all .25s linear;
   }
 }
 
 .container {
   padding: 8px;
+}
+
+.city {
+  display: flex;
+  align-items: center;
+  flex-wrap: wrap;
+  margin-top: 10px;
+  margin-bottom:10px;
+
+  &-item {
+    padding: 3px 6px;
+    margin-right: 8px;
+    margin-bottom: 8px;
+    border: 1px solid #eee;
+    cursor: pointer;
+  }
+}
+
+.city-name {
+  display: flex;
+  align-items: center;
+  flex-wrap: wrap;
+
+  .city-name-item {
+    margin-right: 6px;
+    margin-bottom: 6px;
+  }
+}
+
+.rotate {
+  transform: rotate(180deg);
 }
 </style>
