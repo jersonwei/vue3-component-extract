4a018938292497a03c4c03acdd04d4819d81a4ef
王伟 <872937094@qq.com>
2022-06-12 14:54:38 +0800

    ✨ feat(list): 使用PropType定义prop类型

--- src/components/list/src/index.vue
+++ src/components/list/src/index.vue
@@ -1,5 +1,18 @@
 <script setup lang='ts'>
-
+import { PropType } from 'vue'
+import type { ListOptions, ActionOptions } from './type'
+let props = defineProps({
+  // 当前列表的内容
+  list: {
+    type: Array as PropType<ListOptions[]>,
+    required: true
+  },
+  // 操作内容
+  actions: {
+    type: Array as PropType<ActionOptions[]>,
+    default: () => []
+  }
+})
 </script>
 <template>
   <div>
