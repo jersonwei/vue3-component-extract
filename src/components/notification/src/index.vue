7eec5accdb30efb1a7ee6d27e8361dbc67b0b1cf
王伟 <872937094@qq.com>
2022-06-12 14:32:15 +0800

    ✨ feat(notification): 引入弹出框组件包裹提示组件

--- src/components/notification/src/index.vue
+++ src/components/notification/src/index.vue
@@ -1,10 +1,37 @@
 <script setup lang='ts'>
+import { toLine } from '../../../utils';
 
+let props = defineProps({
+  // 显示的图标
+  icon: {
+    type: String,
+    default: 'Bell'
+  },
+  // 通知数量
+  value: {
+    type: [String, Number],
+    default: ''
+  },
+  // 最大值
+  max: {
+    type: Number
+  },
+  // 小圆点
+  isDot: {
+    type: Boolean,
+    default: false
+  }
+})
 </script>
 <template>
-  <div>
-    提示组件
-  </div>
+  <el-popover placement="bottom" title="Title" :width="200" trigger="click"
+    content="this is content, this is content, this is content">
+    <template #reference>
+      <el-badge style="cursor:pointer" :value="value" :max="max" :isDot="isDot">
+        <component :is="`el-icon-${toLine(icon)}`"></component>
+      </el-badge>
+    </template>
+  </el-popover>
 </template>
 <style lang='scss' scoped>
 </style>
